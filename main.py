a8f0ada0a4ca2a3799fe399eb86be525fb4c3948
Amit thapliyal <thapliyalamit2001@gmail.com>
2022-09-21 17:34:30 +0530

    adding a file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
 import os
 from quoteApi import get_quote
+from dotenv import load_dotenv
 
+load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
@@ -24,4 +26,7 @@ async def on_message(message):
     if message.content.startswith('inspire'):
         gQuote = get_quote()
         await message.channel.send(gQuote)
-client.run('TOKEN')
+    if message.content.startswith('file'):
+        file = discord.File('f.txt')
+        await message.channel.send(file=file, content='your file')
+client.run(os.getenv('TOKEN'))
